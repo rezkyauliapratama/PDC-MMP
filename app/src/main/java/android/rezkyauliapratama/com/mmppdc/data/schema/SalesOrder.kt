b7caf9efad8fddd9addc8c3f3ec4525c18a88d4b
Rezky Aulia Pratama <rezkyauliapratama@gmail.com>
2018-10-17 18:02:37 +0700

    add sealed class into Salesorder

--- app/src/main/java/android/rezkyauliapratama/com/mmppdc/data/schema/SalesOrder.kt
+++ app/src/main/java/android/rezkyauliapratama/com/mmppdc/data/schema/SalesOrder.kt
@@ -3,6 +3,11 @@ package android.rezkyauliapratama.com.mmppdc.data.schema
 /**
  * Created by Rezky Aulia Pratama on 16/10/18.
  */
+sealed class Attr {
+    val isExpandable: Boolean = false
+    val totalItem: Int = 0
+}
+
 data class SalesOrder(
         val id: String,
         val so_number: String,
@@ -11,5 +16,11 @@ data class SalesOrder(
         val customer_category : String,
         val customer_type : String,
         val total_credit : String,
-        val credit_term : String
-)
+        val credit_term : String,
+        val approve_status: String,
+        val user_email: String,
+        val created_date: String,
+        val approve_date: String,
+        val erp_response: String,
+        val items: List<Item>
+): Attr()
---
+++ app/src/main/java/android/rezkyauliapratama/com/mmppdc/data/schema/User.kt
@@ -0,0 +1,9 @@
+package android.rezkyauliapratama.com.mmppdc.data.schema
+
+data class User(
+        val id: String,
+        val name: String,
+        val email: String,
+        val pin: String,
+        val password: String
+)
